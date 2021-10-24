8d07791841e36ca06c0aecff4d633dd3eee73df2
J-Caques <89541215+J-Caques@users.noreply.github.com>
2021-10-24 19:09:59 -0400

    pseudo code w/ tests

--- app1/src/main/java/baseline/Application.java
+++ app1/src/main/java/baseline/Application.java
@@ -1,32 +1,22 @@
 package baseline;
 
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 1 Solution
+ *  Copyright 2021 Jonathan Caques
+ */
+
 public class Application {
-    /*
-    The application shall be able to manage at least 256 unique todo lists
 
-A todo list shall have a title
-A title shall be at least 3 characters long
-A todo list shall have the capacity to store at least 256 unique items
-An item shall have a description
-An item shall have a due date, formatted as YYYY-MM-DD
+    public static void main(String[] args) {
 
-A user shall be able to add a new todo list
-A user shall be able to remove an existing todo list
-A user shall be able to edit the title of an existing todo list
+        /*
+            Start() Application.fxml
+                if Edit List selected start() Edit List.fxml
+                access EditList Methods
 
-A user shall be able to add a new item to an existing todo list
-A user shall be able to remove an item from an existing todo list
-A user shall be able to edit the description of an item within an existing todo list
-A user shall be able to edit the due date of an item within an existing todo list
-A user shall be able to mark an item in a todo list as complete
-A user shall be able to display all of the existing items in a todo list
-A user shall be able to display only the incompleted items in a todo list
-A user shall be able to display only the completed items in a todo list
-A user shall be able to save todo lists to local storage
-A user shall select which lists will be saved
-A user shall specify the file location and name for the saved data
-All selected lists shall be saved to a single text file
-A user shall be able to load previously saved todo list data
-
-     */
+                if New List selected, start() New List.fxml
+                access ListCreator methods
+                access EditItems methods
+         */
+    }
 }
